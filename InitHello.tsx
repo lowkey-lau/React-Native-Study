d0d839ab4a8eb7238771d369f99751aba7ce9e13
lowkey <a317605747@gmail.com>
2023-02-02 11:15:53 +0800

    ddd

--- InitHello.tsx
+++
@@ -1,9 +0,0 @@
-import {Text, View} from 'react-native';
-
-export default function InitHello() {
-  return (
-    <View>
-      <Text>InitHeasdasdasdll123123213o12412421</Text>
-    </View>
-  );
-}
